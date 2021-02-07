8ca7439bd1c3682db8a9e7dbf81f915d962b98da
Lucas Vargas <lucasjv92@gmail.com>
2021-02-07 12:09:39 -0300

    Upload quickstart gmail api .net

--- GAFEAPI/Controllers/AttachmentController.cs
+++ GAFEAPI/Controllers/AttachmentController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,10 +33,56 @@ namespace GAFEAPI.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            var stream =
-                new FileStream("credentials.json", FileMode.Open, FileAccess.Read);
-            
-            return stream.Length.ToString();
+            // If modifying these scopes, delete your previously saved credentials
+            // at ~/.credentials/gmail-dotnet-quickstart.json
+            string[] Scopes = { GmailService.Scope.GmailReadonly };
+            string ApplicationName = "Gmail API .NET Quickstart";
+
+            var response = "";
+           
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                response += "Credential file saved to: " + credPath + Environment.NewLine;
+            }
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+
+            // List labels.
+            IList<Label> labels = request.Execute().Labels;
+            response += "Labels:" + Environment.NewLine;
+            if (labels != null && labels.Count > 0)
+            {
+                foreach (var labelItem in labels)
+                {
+                    response += string.Format("{0}", labelItem.Name) + Environment.NewLine;
+                }
+            }
+            else
+            {
+                response += "No labels found.";
+            }
+
+            return response;
         }
 
         // POST api/<AttachmentController>

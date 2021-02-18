e1a5b29ab09d46345eabc67688a06331ef5eee86
Lucas Vargas <lucasjv92@gmail.com>
2021-02-18 12:29:34 -0300

    Return PDF in browser of first attachment of mail

--- GAFEAPI/Controllers/AttachmentController.cs
+++ GAFEAPI/Controllers/AttachmentController.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
 using MailKit.Net.Imap;
 using MailKit;
 using MailKit.Search;
@@ -52,13 +48,14 @@ namespace GAFEAPI.Controllers
             var items = MessageSummaryItems.BodyStructure | MessageSummaryItems.UniqueId;
             var matched = new UniqueIdSet();
 
-            foreach (var message in inbox.Fetch(results, items))
+            foreach (var messageSummary in inbox.Fetch(results, items))
             {
                 //Has Attachment
-                if (message.BodyParts.Any(x => x.IsAttachment))
+                if (messageSummary.BodyParts.Any(x => x.IsAttachment))
                 {
-                    response += message.UniqueId + Environment.NewLine;
-                    matched.Add(message.UniqueId);
+                    var message = await inbox.GetMessageAsync(messageSummary.UniqueId);
+                    response += string.Format("{0} {1} {2}",messageSummary.UniqueId,message.Subject,Environment.NewLine);
+                    matched.Add(messageSummary.UniqueId);
                 }                    
             }
 
@@ -86,7 +83,9 @@ namespace GAFEAPI.Controllers
 
             uint.TryParse(id, out var uid);
             var uniqueId = new UniqueId(uid);
-            response += inbox.GetMessage(uniqueId);
+            var message = inbox.GetMessage(uniqueId);
+
+            response += message.ToString();
 
             //Mark as read
             inbox.AddFlags(uniqueId, MessageFlags.Seen, true);
@@ -99,51 +98,89 @@ namespace GAFEAPI.Controllers
 
         // GET: api/<AttachmentController>/id
         [HttpGet("GetAttachment/{id}")]
-        public async Task<string> GetAttachment(string id)
+        public async Task<IActionResult> GetAttachment(string id)
         {
-            var response = "";
-
-            var client = new ImapClient();
-            client.Connect(_imapConfiguration.Server, _imapConfiguration.Port, _imapConfiguration.UseSSL);
-            client.Authenticate(_imapConfiguration.Email, _imapConfiguration.Password);
-
-            // The Inbox folder is always available on all IMAP servers...
-            var inbox = client.Inbox;
-            inbox.Open(FolderAccess.ReadWrite);
-
-            uint.TryParse(id, out var uid);
-
-            var message = inbox.GetMessage(new UniqueId(uid));
-
-            foreach (var attachment in message.Attachments)
+            try
             {
-                var fileName = attachment.ContentDisposition?.FileName ?? attachment.ContentType.Name;
+                var client = new ImapClient();
+                client.Connect(_imapConfiguration.Server, _imapConfiguration.Port, _imapConfiguration.UseSSL);
+                client.Authenticate(_imapConfiguration.Email, _imapConfiguration.Password);
 
-                response += fileName + Environment.NewLine;
+                // The Inbox folder is always available on all IMAP servers...
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadWrite);
 
-                /*
-                using (var stream = File.Create(fileName))
+                uint.TryParse(id, out var uid);
+
+                var message = inbox.GetMessage(new UniqueId(uid));
+
+                if (message.Attachments.Any())
                 {
+                    var attachment = message.Attachments.FirstOrDefault();
+
+                    var fileName = attachment.ContentDisposition?.FileName ?? attachment.ContentType.Name;
+
+                    var stream = new MemoryStream();
+                    
                     if (attachment is MessagePart)
                     {
                         var rfc822 = (MessagePart)attachment;
 
-                        rfc822.Message.WriteTo(stream);
+                        await rfc822.Message.WriteToAsync(stream);
                     }
                     else
                     {
                         var part = (MimePart)attachment;
 
-                        part.Content.DecodeTo(stream);
+                        await part.Content.DecodeToAsync(stream);
                     }
+
+                    client.Disconnect(true);
+
+                    var mimeType = "application/pdf";
+
+                    var content = stream.ToArray();
+                    return File(content, contentType: mimeType);
+                                        
                 }
-                */
+                else
+                {
+                    return NotFound();
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+
+        }
+
+        private void ReadMessage()
+        {
+            /*
+            var message = folder.GetMessage(uid);
+
+            // here's how to get the Subject
+            string subject = message.Subject;
+
+            // here's how to get the body (in the simple way)
+            string text = message.TextBody;
+            string html = message.HtmlBody;
+
+            // here's how to get *just* attachments:
+            foreach (MimeEntity attachment in message.Attachments)
+            {
+                // ...
             }
 
-            client.Disconnect(true);
-
-            return response;
-
+            // here's how to get both attachments and inline attachments:
+            foreach (MimeEntity attachment in message.BodyParts)
+            {
+                // ...
+            }
+            */
         }
     }
 }
